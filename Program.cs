e7eada97cb98839a3f3d0ac9a378a19dcf360fb2
Illia Rybenok <iliarybenok@gmail.com>
2023-05-23 13:25:04 +0300

    lab4
    lab4

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введiть шлях до каталогу: ");
+        string directoryPath = Console.ReadLine(); // Вкажіть шлях до каталогу, для якого хочете підрахувати кількість файлів
+
+        CountFilesInSubdirectories(directoryPath);
+
+        Console.ReadLine();
+    }
+
+    static void CountFilesInSubdirectories(string directoryPath)
+    {
+        try
+        {
+            DirectoryInfo directory = new DirectoryInfo(directoryPath);
+            FileInfo[] files = directory.GetFiles("*.*", SearchOption.AllDirectories);
+
+            foreach (FileInfo file in files)
+            {
+                string subdirectory = file.DirectoryName;
+                int fileCount = Directory.GetFiles(subdirectory, "*", SearchOption.AllDirectories).Length;
+                Console.WriteLine($"Кiлькiсть файлiв у пiдкаталозi {subdirectory}: {fileCount}");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Помилка при пiдрахунку файлiв: {ex.Message}");
+        }
+    }
+}
